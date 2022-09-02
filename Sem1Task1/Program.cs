4810dc19cd0a8c151b51f62b2fecfe66d2b6ebee
infurgigh <infurgigh@mail.ru>
2022-09-02 13:40:56 +0300

    Программа определения квадрата числа

---
+++ Sem1Task1/Program.cs
@@ -0,0 +1,19 @@
+﻿//========================================================
+// Напишите программу которая на вход принимает два числа
+// и проверяет, является ли первое число квадратом второго.
+// =======================================================
+
+
+string? inputLineA = Console.ReadLine();
+string? inputLineB = Console.ReadLine();
+
+if (inputLineA != null && inputLineB != null)
+{
+    int inputNumberA = int.Parse(inputLineA);
+    int inputNumberB = int.Parse(inputLineB);
+
+    bool outRusult = (inputNumberA * inputNumberA == inputNumberB);
+
+
+    Console.WriteLine(outRusult);
+}
