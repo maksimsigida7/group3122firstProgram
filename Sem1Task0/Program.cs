bcc9906421781bff68447604d6d2b6af589b8127
infurgigh <infurgigh@mail.ru>
2022-09-02 13:30:38 +0300

    Рассчет квадрата и времени пересчета

---
+++ Sem1Task0/Program.cs
@@ -0,0 +1,22 @@
+﻿//========================================================
+// Напишите программу которая на выход принимает число и 
+//  выдает его квадрат (число умноженное на само себя).
+// =======================================================
+
+
+string? inputLine = Console.ReadLine();
+if (inputLine != null)
+{
+    int inputNumber = int.Parse(inputLine);
+    int outNumber = 0;
+    DateTime d1 = DateTime.Now;
+for (int i = 0; i < 10000000; i++)
+    {
+        outNumber = inputNumber * inputNumber;
+        // int outNumber = (int)Math.Pow(inputNumber,2);
+    }
+    DateTime d2 = DateTime.Now;
+    Console.WriteLine(d2 - d1);
+    
+    Console.WriteLine(outNumber);
+}
