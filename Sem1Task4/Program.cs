0603419dc231a042e76dd720c5717316734426fe
infurgigh <infurgigh@mail.ru>
2022-09-02 18:08:49 +0300

    Новая программа по поиску паксимального числа

---
+++ Sem1Task4/Program.cs
@@ -0,0 +1,32 @@
+﻿//========================================================
+// #4 Напишите программу которая на вход принимает три числа 
+// и выдает максимально е значение из этих числел.
+// =======================================================
+
+
+string? inputLineA = Console.ReadLine(); //считали первое число от пользователя 
+string? inputLineB = Console.ReadLine(); //считали второе число от пользователя
+string? inputLineX = Console.ReadLine(); //считали третье число от пользователя 
+
+        
+if (inputLineA != null && inputLineB != null && inputLineX != null) // проверили что было не было пустым
+
+{
+    int inputNumberA = int.Parse(inputLineA); // превратили значение в целое число
+    int inputNumberB = int.Parse(inputLineB); // превратили значение в целое число
+    int inputNumberX = int.Parse(inputLineX); // превратили значение в целое число
+
+
+ if (inputNumberA > inputNumberB && inputNumberA > inputNumberX)
+    {
+        Console.WriteLine("Число A Максимальное");
+    }
+        else
+        if (inputNumberB > inputNumberA && inputNumberB > inputNumberX)
+        {
+            Console.WriteLine("Число B Максимальное");
+        }
+            else 
+            Console.WriteLine("Число X Максимальное");
+}
+    
