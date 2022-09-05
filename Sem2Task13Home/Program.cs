f95de8e4cea7fb29812d8bfa165cf91a4c96bcc5
infurgigh <infurgigh@mail.ru>
2022-09-05 22:17:43 +0300

    Добавил изменения в новую программу

---
+++ Sem2Task13Home/Program.cs
@@ -0,0 +1,34 @@
+﻿// Напишите программу, которая выводит третью цифру
+// заданного числа или сообщает, что третьей цифры нет.
+
+void Variant1()
+//Вариант только с 3-х значными числами.
+{
+    Console.Write("Введите число: ");
+string? inputLineA = Console.ReadLine();
+int inputNumberA = int.Parse(inputLineA);
+
+if (inputNumberA > 100 && inputNumberA < 1000)
+{
+        //int firstDigit = inputNumberA / 100; // Получили старшее число
+        int lastDigit = inputNumberA % 10; //определили число младшего разряда
+        Console.WriteLine("Третья цифра заданного числа: " + lastDigit);
+    }
+else
+{
+        Console.WriteLine("Число не 3-х значное");
+    }
+}
+// ===========================================================================
+
+void Variant2()
+// Вариант с неограниченным числом цифр.
+
+{
+    Console.Write("Введите число: ");
+    string? inputLineA = Console.ReadLine();
+    int inputNumberA = int.Parse(inputLineA);
+
+    char[] charArray = inputNumberA.ToString().ToCharArray(); // Превращаем число в массив символов
+    Console.WriteLine(charArray[2]);
+}
