16a62622806e565fd3eaee1ee472288ab48cb212
infurgigh <infurgigh@mail.ru>
2022-09-05 23:18:40 +0300

    Написали программу которая по дню недели определяет выходной это или нет

---
+++ Sem2Task15Home/Program.cs
@@ -0,0 +1,28 @@
+﻿// Напишите программу, которая принимает на вход цифру, 
+// обозначающую день недели, и проверяет, является ли этот день выходным.
+//============================================================================
+
+
+    Console.Write("Введите число: ");
+    string? inputLineA = Console.ReadLine();
+    int inputNumberA = int.Parse(inputLineA);
+
+    if (inputNumberA == 6 || inputNumberA == 7)
+    {
+        Console.WriteLine("День недели - Выходной ");
+    }
+    else
+    {
+        if (inputNumberA > 7)
+        {
+            Console.WriteLine("Такого дня недели не существует ");
+        }
+        else
+        {
+            Console.WriteLine("День недели - не выходной");
+        }
+
+    }
+
+
+
