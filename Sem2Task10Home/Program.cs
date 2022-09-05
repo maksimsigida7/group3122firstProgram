e587a3cf16bf6a577f446acff3d00a96a0382ac9
infurgigh <infurgigh@mail.ru>
2022-09-05 20:26:36 +0300

    Добавил программу которая которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа

---
+++ Sem2Task10Home/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число
+// и на выходе показывает вторую цифру этого числа.
+
+
+Console.Write("Введите число: ");
+string? inputLineA = Console.ReadLine();
+int inputNumberA = int.Parse(inputLineA);
+
+if (inputNumberA > 100 && inputNumberA < 1000)
+{
+    int firstDigit = inputNumberA / 10; // Получили старшее число
+    int NumberX = firstDigit%10; // сложили цифры старшего и младшего числа
+    Console.WriteLine("Вторая цифра заданного числа: " + NumberX);
+}
+else
+{
+    Console.WriteLine("Число не 3-х значное");
+}
