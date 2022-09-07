a16bd88df4b9800e886160d2841d3c3414ac0baf
infurgigh <infurgigh@mail.ru>
2022-09-07 13:14:25 +0300

    Добавили программу по охределению расположения точки по оси координат

---
+++ Sem3Task17/Program.cs
@@ -0,0 +1,42 @@
+﻿// Чтение данных с консоли
+int ReadData(string line)
+{
+    // Выводим сообщение
+    Console.WriteLine(line);
+    // Считываем число
+    int number = int.Parse(Console.ReadLine() ?? "0");
+    //Возвращаем значение
+    return number;
+}
+
+void PrintResult(string line)
+{
+    Console.WriteLine(line);
+}
+
+int QuoterTest(int x, int y)
+{
+    if (x > 0 && y > 0)
+        return 1;
+    if (x < 0 && y > 0)
+        return 2;
+    if (x < 0 && y < 0)
+        return 3;
+    if (x > 0 && y < 0)
+        return 4;
+
+    return -1;
+
+}
+
+void PrintData(string line)
+
+{
+    Console.WriteLine(line);
+}
+
+int x = ReadData("Введите координату X: ");
+int y = ReadData("Введите координату Y: ");
+int res = QuoterTest(x, y);
+
+PrintResult("Точка находится в четверти номер " + res);
