e1f48a9721210309e0d36ff279bb8e66043d8f12
infurgigh <infurgigh@mail.ru>
2022-09-07 13:36:55 +0300

    добавили программу которая показывает координаты при введении четверти

---
+++ Sem3Task18/Program.cs
@@ -0,0 +1,42 @@
+﻿// Напишите программу, которая по заданному номеру
+// четверти, показывает диапазон возможных
+// координат точек в этой четверти (x и y)
+//=================================================================
+
+// Чтение данных с консоли
+int ReadData(string line)
+{
+    // Выводим сообщение
+    Console.WriteLine(line);
+    // Считываем число
+    int number = int.Parse(Console.ReadLine() ?? "0");
+    //Возвращаем значение
+    return number;
+}
+
+
+string QuoterBorderAsk(int numQuoter)
+{
+    if (numQuoter == 1)
+        return "x > 0 y > 0";
+    if (numQuoter == 2)
+        return "x < 0 y > 0";
+    if (numQuoter == 3)
+        return "x < 0 y < 0";
+    if (numQuoter == 4)
+       return "x > 0 y > 0";
+
+    return "";
+
+}
+
+void PrintResult(string line)
+
+{
+    Console.WriteLine(line);
+}
+
+int num = ReadData("Введите номер четверти: ");
+string res = QuoterBorderAsk(num);
+
+PrintResult(res);
