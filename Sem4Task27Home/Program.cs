be2d3762fd749f3d4009b52949d71cb007628775
infurgigh <infurgigh@mail.ru>
2022-09-12 21:39:44 +0300

    Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.

---
+++ Sem4Task27Home/Program.cs
@@ -0,0 +1,39 @@
+﻿// Напишите программу, которая принимает на вход 
+// число и выдаёт сумму цифр в числе.
+
+// Метод считывания данных пользователя
+int ReadData(string line)
+{
+    // Выводим сообщение
+    Console.Write(line);
+    // Считываем число
+    int number = int.Parse(Console.ReadLine() ?? "0");
+    // Возвращаем значение
+    return number;
+}
+
+// Метод, принимает строку, выводит в консоль
+void PrintResult(string line)
+{
+    Console.WriteLine(line);
+}
+
+
+
+int SumDigits(int num)
+{
+    int sum = 0;
+    while (num > 0)
+    {
+        sum += num%10;
+        num = num / 10;
+    }
+    return sum;
+}
+
+int number = ReadData("Введите число: ");
+
+DateTime d1 = DateTime.Now;
+int sumOfDigits = SumDigits(number);
+Console.WriteLine(DateTime.Now - d1);
+PrintResult("Сумма цифр в числе: " + sumOfDigits);
