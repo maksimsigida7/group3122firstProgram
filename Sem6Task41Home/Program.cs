8e65639a2a5635abe758660d720843a8ecb6ce5b
infurgigh <infurgigh@mail.ru>
2022-09-22 01:38:05 +0300

    Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем

---
+++ Sem6Task41Home/Program.cs
@@ -0,0 +1,41 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте,
+// сколько чисел больше 0 ввёл пользователь.
+
+// Чтение данных из консоли
+int ReadData(string line)
+{
+    // Выводим сообщение
+    Console.WriteLine(line);
+    // Считываем число
+    int number = int.Parse(Console.ReadLine() ?? "0");
+    // Возвращаем значение
+    return number;
+}
+
+// Печать результата
+void PrintResult(string line)
+{
+    Console.WriteLine(line);
+}
+
+int Count(int M)
+{
+    int res = 0;
+    for (int i = 0; i < M; i++)
+        {
+            int num = ReadData("Введите число ");
+            {
+                if (num > 0) 
+                {
+                    res++;
+                }
+            }
+        }
+    
+    return res;
+}
+
+int num = ReadData("Введи число М (количество чисел): ");
+
+int numbCount = Count(num);
+PrintResult("Количество чисел больше 0: " + numbCount);
