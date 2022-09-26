f7c6dc902129a0e57b55352013aeef6ad80fdca3
infurgigh <infurgigh@mail.ru>
2022-09-27 02:00:24 +0300

    Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.

---
+++ Sem7Task47Home/Program.cs
@@ -0,0 +1,41 @@
+﻿// Задайте двумерный массив размером m×n, 
+// заполненный случайными вещественными числами.
+
+void Fill2DArrayRandNumb(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = Convert.ToDouble(new Random().Next(-10, 100)) / 10;
+        }
+    }
+}
+
+ConsoleColor[] col = new ConsoleColor[]{ConsoleColor.Blue,ConsoleColor.Cyan,
+                                        ConsoleColor.DarkBlue,ConsoleColor.DarkCyan,ConsoleColor.DarkGray,
+                                        ConsoleColor.DarkGreen,ConsoleColor.DarkMagenta,ConsoleColor.DarkRed,
+                                        ConsoleColor.DarkYellow,ConsoleColor.Gray,ConsoleColor.Green,
+                                        ConsoleColor.Magenta,ConsoleColor.Red,ConsoleColor.White,
+                                        ConsoleColor.Yellow};
+
+void PrintArray(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.ForegroundColor = col[new System.Random().Next(0, 15)];
+            Console.Write(array[i, j] + "\t");
+        }
+
+        Console.WriteLine("");
+    }
+}
+
+int m = 10;
+int n = 5;
+double[,] numbers = new double[m, n];
+Fill2DArrayRandNumb(numbers);
+PrintArray(numbers);
---
+++ Sem7Task52Home/Program.cs
@@ -0,0 +1,51 @@
+﻿// Задайте двумерный массив из целых чисел. 
+// Найдите среднее арифметическое элементов в каждом столбце.
+
+Console.Write("Введите количество строк: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите количество столбцов: ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+int[,] numbers = new int[n, m];
+FillArrayRandomNumbers(numbers);
+
+//Генерируем массив
+void FillArrayRandomNumbers(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(0, 10);
+        }
+    }
+}
+
+//Распечатываем массив
+void PrintArray(int[,] array)
+{
+
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " " + "\t");
+        }
+        Console.WriteLine("");
+    }
+}
+
+//Поиск и печать среднего арифметического
+for (int j = 0; j < numbers.GetLength(1); j++)
+{
+    double avarage = 0;
+    for (int i = 0; i < numbers.GetLength(0); i++)
+    {
+        avarage = (avarage + numbers[i, j]);
+    }
+    avarage = avarage / n;
+    Console.Write(avarage + "\t" );
+}
+Console.WriteLine("- Среднее арифметическое значение");
+Console.WriteLine("-------------------------------------------------");
+PrintArray(numbers);
